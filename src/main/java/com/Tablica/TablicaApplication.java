2ccffc2c03b84bc41e0de6dda7d2a61b55434789
Lachuuuu <lach.radoslaw.0@gmail.com>
2022-08-13 19:38:59 +0200

    init

---
+++ src/main/java/com/Tablica/TablicaApplication.java
@@ -0,0 +1,13 @@
+package com.Tablica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TablicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tablica/TablicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tablica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
